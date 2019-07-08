6ee0f02546bc3d7eb1238da304d800f9b5b2bf4e
ymiaoy <794326512@qq.com>
2019-07-08 09:08:14 +0800

    .sh

---
+++ add_marker.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch"&
+sleep 3
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch"&
+sleep 3
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch"
