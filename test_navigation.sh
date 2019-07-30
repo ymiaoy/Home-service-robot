3d67d88e86762cd3867406b735f7ccde142a1ed6
ymiaoy <794326512@qq.com>
2019-07-30 16:32:47 +0800

    1.1

--- test_navigation.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch"&
-sleep 5
-xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch"&
-sleep 5
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch"
-
-
--- test_slam.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch"&
-sleep 3
-xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch"&
-sleep 3
-xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
-sleep 3
-xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch"
