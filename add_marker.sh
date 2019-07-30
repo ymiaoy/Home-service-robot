59304637254c357ac0783d696dd62b76d0a78d29
Miao <794326512@qq.com>
2019-07-30 16:30:39 +0800

    Delete add_marker.sh

--- add_marker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch"&
-sleep 3
-xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch"&
-sleep 3
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch"
